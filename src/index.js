9c6493627f6031c5d4fc02d023292c1a45c489ed
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2024-01-01 15:51:41 -0300

    Ok

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatar-img"
+      src="img/ninho-do-arigo.png"
+      alt="Ninho do Arigó"
+    />
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h2>Leandro Junior Alves Dos Santos</h2>
+      <p>
+        Multi-skilled professional, has abilities with teaching, painting and
+        programming....
+      </p>
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div style={{ backgroundColor: props.color }}>
+      <span>{props.Skill}</span>
+      <span>{props.emogi}</span>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill Skill="Java" emogi="\O/" color="orange" />
+      <Skill Skill="Graffiti" emogi=":)" color="red" />
+      <Skill Skill="React Js" emogi=":|" color="grey" />
+      <Skill Skill="DotNet" emogi="|O" color="blue" />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
